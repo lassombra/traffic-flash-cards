8729785e4dfa8579f660931389a71cf8363c92a8
brian <briatt22@gmail.com>
2019-06-02 01:08:52 -0500

    Move "deck view state" into a monitor middleware

--- studySession/monitor.js
+++ studySession/monitor.js
@@ -1,6 +1,10 @@
 import {resultMonitor} from '../state/middleWare';
+import {VIEW} from './constants';
+import {Actions} from '../viewDeck';
 
 function monitorFinished(prevState, state, dispatch) {
-    console.log(prevState, state);
+    if (state.view === VIEW && state.deck && state.deck.length == 0 && state.cycled_deck && state.cycled_deck == 0 && !state.activeCard) {
+        dispatch(Actions.switchToDeckView());
+    }
 }
 resultMonitor.add(monitorFinished);
--- viewDeck/reducers/index.js
+++ viewDeck/reducers/index.js
@@ -1,6 +1,5 @@
 import switchToDeckView from './switchToDeckView';
 import removeCard from './removeCard';
-export deckViewState from './deckViewState';
 
 export default function deckViewComposite(state, action) {
     state = switchToDeckView(state, action);
