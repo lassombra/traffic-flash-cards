a59141b0726f837262ff4cd88c3f469c0654610f
brian <briatt22@gmail.com>
2019-06-07 21:43:10 -0500

    Initial setup for react native firebase

--- android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -3,6 +3,6 @@ package host.exp.exponent.generated;
 // This file is auto-generated. Please don't rename!
 public class DetachBuildConstants {
 
-  public static final String DEVELOPMENT_URL = "exp3fdd032cedb84c96855534831617df39://192.168.0.16:19000";
+  public static final String DEVELOPMENT_URL = "lassombratrafficflash://192.168.0.16:19000";
 
 }
